6ff45ea0c2e72f4bc758667ad1df07057e62d2a8
Charly98cma <1998cma@gmail.com>
2021-02-21 12:02:37 +0100

    Added world generation, operational loop and ticks
    - Added backbone (gameoflife.py)
    - 'tick' function, that changes each cell simultaneously
    - Changed 'next_gen' to 'newGen'
    - Solved bug with 'newGen' logic

--- gameoflife.py
+++ gameoflife.py
@@ -1,60 +1,48 @@
-"""
-Returns the changes that must be applied on the next generation as a tick
+from sys import exit
+from time import time
 
-Parameters
-----------
-prev_gen : [[Cell, ...], ...]
+from cell import Cell
+import gen_changes
 
-Returns
--------
-changes : [{"coords" : (X,Y) , "state" : 'L|D'}]
-"""
-def new_gen(prev_gen) -> list:
-    # Empty list of changes
-    changes = []
-    # Check each cell on the system
-    for cell in [item for row in prev_gen for item in row]:
-        x,y = cell.getCoords()
-        adj_coords = [(x-1, y-1), (x, y-1), (x+1, y-1),
-                      (x-1, y  ),           (x+1, y  ),
-                      (x-1, y-1), (x, y+1), (x+1, y+1)]
-        adj_cells = [(a,b) for a,b in adj_coords if a > 0 and b > 0]
-        # Alive neighbour cell counter
-        n_neigh = 0
-        # Iterate trought the neighbours cells
-        for neigh_x, neigh_y in adj_cells:
-            # Get neighbour cell
-            neigh_cell = prev_gen[neigh_x][neigh_y]
-            # Check neighbour cell state
-            if neigh_cell.getState() == 'L':
-                n_neigh += 1
-        # Kill cell if overpopulation or starvation
-        if cell.getState() == 'L' and n_neigh not in [2,3]:
-            changes = add_change(changes, (x,y), 'D')
-        # Revive a cell
-        elif cell.getState() == 'D' and n_neigh in [2,3]:
-            changes = add_change(changes, (x,y), 'L')
-    return changes
+class GameOfLife:
 
-"""
-Given the changes list, appends the new change with the given parameters.
-(Used to make the code much more readable)
+    def __init__(self, dims):
+        self.dims = dims
+        self.world = []
+        self.genWorld()
+        self.loop()
 
-Parameters
-----------
-changes : list(dict) -> List of dictionaries stating the changes on the system
-coords : tuple  -> Coordinated of the cell => (X,Y)
-state  : string -> New state of the cell   => 'L' (lives) OR 'D' (dies)
+    def genWorld(self):
+        for i in range(self.dims):
+            # Generate row
+            self.world.append([])
+            # Fill each position with a dead cell
+            for j in range(self.dims):
+                self.world[i].append(Cell((i,j),'D'))
 
-Returns
--------
-changes : list(dict) -> List of dictionaries with the new changed
-"""
-def add_change(changes, coords, state) -> list:
-    changes.append(
-        {
-            "coords" : coords,
-            "state"  : state
-        }
-    )
-    return changes
+    def loop(self):
+        while True:
+            # start_time = time.time()
+            gen_changes.tick(
+                self.world,
+                gen_changes.newGen(self.world, self.dims)
+            )
+            # print("--- New gen transition in: %s seconds ---"
+            #       % (time.time() - start_time))
+
+
+def main():
+    print("""Welcome to my...
+
+     ██████   █████  ███    ███ ███████      ██████  ███████     ██      ██ ███████ ███████
+     ██       ██   ██ ████  ████ ██          ██    ██ ██          ██      ██ ██      ██
+      ██   ███ ███████ ██ ████ ██ █████       ██    ██ █████       ██      ██ █████   █████
+       ██    ██ ██   ██ ██  ██  ██ ██          ██    ██ ██          ██      ██ ██      ██
+         ██████  ██   ██ ██      ██ ███████      ██████  ██          ███████ ██ ██      ███████
+    """)
+
+    dims = int(input("Now, please, enter the dimensions: "))
+    GameOfLife(dims)
+
+if __name__ == "__main__":
+    main()
---
+++ gen_changes.py
@@ -0,0 +1,72 @@
+"""
+Returns the changes that must be applied on the next generation as a tick
+
+Parameters
+----------
+prev_gen : [[Cell, ...], ...]
+dims     : integer - Number of cells on each row
+
+Returns
+-------
+changes : [{"coords" : (X,Y) , "state" : 'L|D'}]
+"""
+def newGen(prev_gen, dims) -> list:
+    # Empty list of changes
+    changes = []
+    # Check each cell on the system
+    for cell in [item for row in prev_gen for item in row]:
+        x,y = cell.getCoords()
+        adj_coords = [(x-1, y-1), (x, y-1), (x+1, y-1),
+                      (x-1, y  ),           (x+1, y  ),
+                      (x-1, y-1), (x, y+1), (x+1, y+1)]
+        adj_cells = [(a,b) for a,b in adj_coords
+                     if a in range(dims) and b in range(dims)]
+        # Alive neighbour cell counter
+        n_neigh = 0
+        # Iterate trought the neighbours cells
+        for neigh_x, neigh_y in adj_cells:
+            # Get neighbour cell
+            neigh_cell = prev_gen[neigh_x][neigh_y]
+            # Check neighbour cell state
+            if neigh_cell.getState() == 'L':
+                n_neigh += 1
+        # Kill cell if overpopulation or starvation
+        if cell.getState() == 'L' and n_neigh not in [2,3]:
+            changes = add_change(changes, (x,y), 'D')
+        # Revive a cell
+        elif cell.getState() == 'D' and n_neigh in [2,3]:
+            changes = add_change(changes, (x,y), 'L')
+    return changes
+
+"""
+Given the changes list, appends the new change with the given parameters.
+(Used to make the code much more readable)
+
+Parameters
+----------
+changes : list(dict) -> List of dictionaries stating the changes on the system
+coords : tuple  -> Coordinated of the cell => (X,Y)
+state  : string -> New state of the cell   => 'L' (lives) OR 'D' (dies)
+
+"""
+def __add_change(changes, coords, state) -> list:
+    changes.append(
+        {
+            "coords" : coords,
+            "state"  : state
+        }
+    )
+
+
+"""
+Applies the generational changes to each cell that will change its state
+
+Parameters
+----------
+prev_gen : [[Cell, ...], ...]
+changes : [{"coords" : (X,Y) , "state" : 'L|D'}]
+"""
+def tick(prev_gen, changes):
+    for chg in changes:
+        x,y = chg["coords"]
+        prev_gen[x][y].setState(chg["state"])
