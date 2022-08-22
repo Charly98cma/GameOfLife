3270fc810332c3bd657301f49967d171ee4bcdb9
Charly98cma <1998cma@gmail.com>
2022-08-22 15:44:31 +0200

    feat: Cell object
    Refs: #2

--- src/models/cell.py
+++ src/models/cell.py
@@ -1,4 +1,17 @@
+class States:
+  DEAD, ALIVE = 'D', 'A'
+
 class Cell:
 
-  def __init__():
-    ...
+  def __init__(self, coords):
+    self._coords = coords
+    self.state = States.DEAD
+
+  def get_coords(self):
+    return self._coords
+
+  def get_state(self):
+    return self._state
+
+  def switch_state(self):
+    self._state = States.DEAD if self.state == States.ALIVE else States.Alive
